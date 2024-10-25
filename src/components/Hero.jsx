e5611b12f47537d930061619126d71cc7f81f769
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-10-25 23:22:26 +0600

    framer motion added in Hero and Navbar section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Bgimage from '../../src/assets/bg-slate.png'
 import blackCoffee from '../../src/assets/black.png'
 import Navbar from './Navbar';
+import {motion} from "framer-motion"
 
 const bgimage = {
     backgroundImage: `url(${Bgimage})`,
@@ -19,23 +20,81 @@ const Hero = () => {
                 <Navbar />
                 <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]'>
                     <div className='text-[#F1DABF] mt-[100px] md:mt-0 p-1 space-y-28'>
-                        <h1 className='text-7xl font-bold leading-tight ml-14'>Coders Cafe</h1>
-                        <div className='relative'> 
+                        <motion.h1 
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ 
+                            type:"spring",
+                            stiffness:100,
+                            damping:10,
+                            delay:0.8
+                         }}
+                        className='text-7xl font-bold leading-tight ml-14'>Coders Cafe</motion.h1>
+                        <motion.div 
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ 
+                            type:"spring",
+                            stiffness:100,
+                            damping:10,
+                            delay:1
+                         }}
+                        className='relative'> 
                             <div className='relative z-10 space-y-4'>
                                 <h1 className='text-2xl'>Blvck Tumbler</h1>  
                                 <h1 className='text-sm opacity-55 leading-loose'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus aspernatur, cumque eos neque dolorem architecto deserunt quis voluptatibus in quisquam quia ducimus!</h1>
                             </div>
                             <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
-                        </div>
+                        </motion.div>
                     </div>
                     <div className='relative'>
-                        <img src={blackCoffee} alt="Coffee-img" className='relative z-40 h-[400px] md:h-[700px] img-shadow'/>
-                        <div className='h-[180px] w-[180px] absolute top-24 -right-16 border-primary border-[20px]  rounded-full z-10'></div>
+                        <motion.img
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        initial={{ opacity: 0, scale:0 }}
+                        animate={{ opacity: 1, scale:1 }}
+                        transition={{ 
+                         type:"spring",
+                         stiffness:100,
+                         damping:10,
+                         delay:0.4
+                        }}
+                        src={blackCoffee} alt="Coffee-img" className='relative z-40 h-[400px] md:h-[700px] img-shadow'/>
+                        <motion.div 
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ 
+                         type:"spring",
+                         stiffness:100,
+                         damping:10,
+                         delay:0.8
+                         }}
+                        className='h-[180px] w-[180px] absolute top-24 -right-16 border-primary border-[20px]  rounded-full z-10'></motion.div>
                         <div className='absolute -top-20 left-[200px] z-[1]'>
-                            <h1 className='text-[170px] scale-150 font-bold text-[#1A1F25]/50 leading-none'>Blvck Lovers</h1>
+                            <motion.h1 
+                            initial={{ opacity: 0, x:-100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ 
+                                type:"spring",
+                                stiffness:100,
+                                damping:10,
+                                delay:.8
+                             }}
+                            className='text-[145px] scale-150 font-bold text-[#1A1F25]/50 leading-none'>Blvck Tumbler</motion.h1>
                         </div>
                     </div>
-                    <div className='text-[#F1DABF] mt-[100px] md:mt-0 p-4 space-y-28'>
+                    <motion.div 
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ 
+                        type:"spring",
+                        stiffness:100,
+                        damping:10,
+                        delay:1
+                     }}
+                    className='text-[#F1DABF] mt-[100px] md:mt-0 p-4 space-y-28'>
                         <h1 className='opacity-0 text-7xl font-bold leading-tight ml-14'>Coders Cafe</h1>
                         <div className='relative'> 
                             <div className='relative z-10 space-y-4'>
@@ -44,7 +103,7 @@ const Hero = () => {
                             </div>
                             <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-gray-900/40'></div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import { SlMenu } from "react-icons/sl";
-
+import {motion} from "framer-motion"
 
 const Navbar = () => {
   return (
-    <nav className='absolute top-0 w-full left-0 pt-10 z-20 text-white'>
+    <nav className='absolute top-0 w-full left-0 pt-10 z-40 text-white'>
         <div className='container'>
             <div className='flex justify-between items-center'>
-                <h1 className='text-2xl font-semibold uppercase'>
-                    <span className='text-[#F1DABF]'>Coders</span>
+                <motion.h1 
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                    type:"spring",
+                    stiffness:100,
+                    damping:10,
+                    delay:0.6
+                 }}
+                className='text-2xl font-semibold uppercase'>
+                    <span className='text-[#F1DABF] pr-3'>Coders</span>
                     <span className='text-primary'>Cafe</span>
-                </h1>
-                <div>
+                </motion.h1>
+                <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ 
+                    type:"spring",
+                    stiffness:100,
+                    damping:10,
+                    delay:0.6
+                 }}>
                     <SlMenu className='text-3xl cursor-pointer'/>
-                </div>
+                </motion.div>
             </div>
         </div>
     </nav>
